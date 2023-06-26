551a13e023f0fae1ba810c947bc4c202d6c36d2c
Sajjad Maleki <saj.maleki@gmail.com>
2023-06-26 21:12:37 +0330

    First Commit

---
+++ 01-Fundamentals/script.js
@@ -0,0 +1,46 @@
+//---------- Linking a JavaScript File----------
+
+//------------ Values and Variables-------------
+
+// Variable name convention
+
+//----------------- Data Types ------------------
+// 7 Primitive data types
+
+//------------ let, const and var-------------
+
+//-------------- Basic Operators---------------
+// Math operators
+
+// Assignment operators
+
+// Comparison operators
+
+//------------ Operator Precedence--------------
+
+//-------- Strings and Template Literals--------
+
+//-------- Type Conversion and Coercion---------
+// type conversion
+
+// type coercion
+
+// Truthy and Falsy Values
+
+// 6 falsy values: 0, '', undefined, null, NaN, false
+
+//------ Equality Operators: loose vs. strict --------
+
+//loose
+
+//strict
+
+//--------------- Logical Operators ------------------
+
+//---------------if / else Statements-----------------
+
+//---------------The switch Statement-----------------
+
+//------------Statements and Expressions--------------
+
+//--------The Conditional (Ternary) Operator----------
