c620a7939edce7121298adf0b798b0d70e90a84a
Sajjad Maleki <saj.maleki@gmail.com>
2023-07-18 00:29:57 +0330

    initialize part 2

---
+++ 02-Arrays/starter/script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+const healthyFoods = [
+  "Water",
+  "Dark Green Vegetables",
+  "Whole Grains",
+  "Beans and Lentils",
+  "Fish",
+  "Berries",
+  "Winter Squash",
+  "Soy",
+  "Flaxseed, Nuts and Seeds",
+  "Organic Yogurt",
+];
+
+const students = [
+  { name: "Edward", score: 21 },
+  { name: "John", score: 37 },
+  { name: "Kate", score: 45 },
+  { name: "Jane", score: 12 },
+  { name: "Tony", score: 13 },
+  { name: "Albert", score: 37 },
+  { name: "John", score: 15 },
+];
+
+
+// length
+
+
+// push
+
+
+// pop
+
+
+// shift
+
+
+
+// unshift
+
+
+// indexOf
+
+
+//_______________Advanced Array Methodes___________
+// at
+
+// reverse
+
+// concat
+
+// forEach
+
+// map
+
+// filter
+
+// find
+
+
+//findLast
+
+
+// findIndex
+
+
+// findLastIndex
+
+
+// some
+
+// every
+
+// flat
+
+
+// flatMap
+
+
+// slice
+
+
+// splice
+
+
+// sort
+
+
+// reduce
+
