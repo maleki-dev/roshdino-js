255feb26d5d887e21648ed12df7383cfcdab40a5
Sajjad <saj.maleki@gmail.com>
2023-08-01 18:34:17 +0330

    complete session 3

--- 02-Arrays and Strings/script.js
+++ 02-Arrays and Strings/script.js
@@ -166,14 +166,23 @@ const studentNames1 = students.map((item) => item.name);
 
 // console.log(studentNames);
 
-console.log(students);
+// console.log(students);
 
-const studentWithGrades = students.map(function (student) {
-  student.grade = claculateGrade(student.score);
-  return student;
-});
+// const studentWithGrades = students.map(function (student) {
+//   student.grade = claculateGrade(student.score);
+//   return student;
+// });
 
-console.log(studentWithGrades);
+const isAnyStudentA = students
+  .map(function (student) {
+    student.grade = claculateGrade(student.score);
+    return student;
+  })
+  .some((student) => student.grade === "A");
+
+console.log("isAnyStudentA", isAnyStudentA);
+
+// console.log(studentWithGrades);
 
 // filter
 
@@ -183,8 +192,8 @@ const studentsWithGradeA = students.filter(function (student) {
 
 const studentsWithGradeB = students.filter((student) => student.grade === "B");
 
-console.log(studentsWithGradeA);
-console.log(studentsWithGradeB);
+// console.log(studentsWithGradeA);
+// console.log(studentsWithGradeB);
 
 // find
 
@@ -192,7 +201,7 @@ const edward = students.find(function (item) {
   return item.name === "Edward";
 });
 
-console.log(edward);
+// console.log(edward);
 
 //findLast
 
@@ -200,41 +209,153 @@ const lastEdward = students.findLast(function (item) {
   return item.name === "Edward";
 });
 
-console.log(lastEdward);
+// console.log(lastEdward);
 
 // findIndex
 
+const edwardIndex = students.findIndex(function (item) {
+  return item.name === "Edwardd";
+});
+
+// console.log("edwardIndex", edwardIndex);
+
 // findLastIndex
+const edwardLastIndex = students.findLastIndex(function (item) {
+  return item.name === "Edward";
+});
+
+// console.log("edwardLastIndex", edwardLastIndex);
 
 // some
 
+const studentsScores = [50, 45, 65, 35, 75, 26, 61];
+
+const didAnyStudentPass = studentsScores.some(function (score) {
+  return score >= 60;
+});
+
+// console.log(didAnyStudentPass);
 // every
+
+const didAllStudentsPass = studentsScores.every((score) => score >= 60);
+
+// console.log(didAllStudentsPass);
 
 // flat
 
+const arr = [1, 2, [3, 4, [5], ["x", [[[[[[[["y"]]]]]]]]]], 6];
+
+// console.log(arr.flat());
+// console.log(arr.flat(2));
+// console.log(arr.flat(Infinity));
+
+const counts = [1, 2, 3]; // [[1, 2, 3], [2, 4, 6], [3, 6, 9]]
+
+const newCounts = counts.map((count) => [count, count * 2, count * 3]).flat();
+
+// console.log("newCounts", newCounts);
+
 // flatMap
+
+const newCountsFlatMap = counts.flatMap((count) => [
+  count,
+  count * 2,
+  count * 3,
+]);
+
+// console.log("newCountsFlatMap", newCountsFlatMap);
 
 // slice
 
+const personNames = ["Ali", "Hassan", "Reza", "Negar", "Amir"];
+
+const slicedNames = personNames.slice(1, 3);
+// console.log("slicedNames", slicedNames);
+
+// console.log(personNames.slice(1));
+// console.log(personNames.slice(-2));
+// console.log(personNames.slice(0, -2));
+// console.log(personNames.slice(2, -2));
+
+const findMiddleIndex = (arr) => {
+  if (arr.length % 2 !== 0) {
+    const middleIndex = Math.ceil(arr.length / 2) - 1;
+    console.log(middleIndex);
+
+    console.log(arr.filter((item, index) => index !== middleIndex));
+  } else {
+    console.log("Array should have odd items!!❤");
+  }
+};
+
+// findMiddleIndex([1, 2, 3, 4, 5, 6, 7]);
+
 // splice
+
+// console.log(personNames.splice(2, 2, "Maryam", "Hosein"));
+
+// console.log(personNames);
 
 // sort
 
+const prices = [23, 45, 12, 56, 98, 34, 123];
+
+const ascendingSortedPrices = prices.sort((a, b) => {
+  return a - b;
+});
+
+console.log("ascendingSortedPrices", ascendingSortedPrices);
+
+const descendingSortedPrices = prices.sort((a, b) => {
+  return b - a;
+});
+
+console.log("descendingSortedPrices", descendingSortedPrices);
+
+const sortedByScoreStudents = students.sort((a, b) => b.score - a.score);
+
+console.log("sortedByScoreStudents", sortedByScoreStudents);
+
 // reduce
 
-// const transactions = [
-//   { description: "Salary", amount: 3000 },
-//   { description: "Rent", amount: -1000 },
-//   { description: "Groceries", amount: -250 },
-//   { description: "Bonus", amount: 500 },
-//   { description: "Utilities", amount: -200 }
-// ];
+//[23, 45, 12, 56, 98, 34, 123]
 
+const totalPrice = prices.reduce(function (acc, item) {
+  return acc + item;
+}, 0);
+
+console.log("totalPrice", totalPrice);
+
+const transactions = [
+  { description: "Salary", amount: 3000 },
+  { description: "Rent", amount: -1000 },
+  { description: "Groceries", amount: -250 },
+  { description: "Bonus", amount: 500 },
+  { description: "Utilities", amount: -200 },
+];
+
+const balance = transactions.reduce(
+  (acc, transaction) => acc + transaction.amount,
+  0
+);
+
+console.log(balance);
 
 /*
 //_____________Exercises_____________
 1. Create an array of sentences from students array like this:
-['Edward has scored 21', 'John has scored 37', ...]
+
+const students = [
+  { name: "Edward", score: 21 },
+  { name: "John", score: 37 },
+  { name: "Kate", score: 45 },
+  { name: "Jane", score: 12 },
+  { name: "Tony", score: 13 },
+  { name: "Edward", score: 37 },
+  { name: "John", score: 15 },
+];
+
+output: ['Edward has scored 21', 'John has scored 37', ...]
 
 
 2. Use the reduce method to count the occurrences of each element in an array.
@@ -279,4 +400,3 @@ const priorityCategories = ["Fruits", "Electronics"];
 // replace
 // replaceAll
 // split
-
