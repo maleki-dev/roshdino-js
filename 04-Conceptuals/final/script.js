13b10939c303ac1543415cdb5449a4bc36e17084
Sajjad Maleki <saj.maleki@gmail.com>
2023-09-08 23:43:36 +0330

    initialize conceptuals

---
+++ 04-Conceptuals/final/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//__________JS Engine and Runtime_____________
+
+
+//________________Scoping_____________________
+
+
+//_______Hoisting and Temporal Dead Zone______
+
+
+//____________The this Keyword________________
+
+
+//___Regular Functions vs. Arrow Functions____
+
+
+//__________ Objects vs. primitives___________
