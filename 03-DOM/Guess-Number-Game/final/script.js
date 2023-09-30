7498291968913b862dd5c3d43f18f60969a4723b
Roshdino <you@example.com>
2023-09-30 18:23:22 +0330

    dom session

--- 03-DOM/Guess-Number-Game/final/script.js
+++ 03-DOM/Guess-Number-Game/final/script.js
@@ -1,11 +1,24 @@
 const checkButton = document.querySelector('.check');
+const againButton = document.querySelector('.again');
 const guessInput = document.querySelector('.guess');
 const messageEl = document.querySelector('.message');
 const scoreEl = document.querySelector('.score');
-const numberEl = document.querySelector('.number')
+const numberEl = document.querySelector('.number');
+const highScoreEl = document.querySelector('.highscore');
 
 let score = 20;
 let highScore = 0;
+
+function setScore(value) {
+    score = value;
+    scoreEl.textContent = value;
+}
+
+function setHighScore(value) {
+    highScore = value;
+    highScoreEl.textContent = value;
+}
+
 const secretNumber = Math.ceil(Math.random()*20)
 
 function displayMessage(message) {
@@ -16,6 +29,15 @@ console.log(secretNumber);
 
 scoreEl.textContent = score;
 
+const resetGame = () => {
+    displayMessage('شروع حدس زدن ...');
+    document.body.style.backgroundColor = '#222';
+    numberEl.style.width = '15rem';
+    setScore(20);
+    guessInput.value = '';
+    numberEl.textContent = '?'
+}
+
 const checkHandler = function() {
     const guess = Number(guessInput.value);
 
@@ -25,11 +47,13 @@ const checkHandler = function() {
             numberEl.style.width = '30rem';
             numberEl.textContent = secretNumber;
             displayMessage('شما برنده شدید!')
+            if(score > highScore) {
+                setHighScore(score);
+            }
         } else {
             const message = guess > secretNumber ? 'بیا پایین تر' : 'برو بالاتر';
             displayMessage(message);
-            score -= 1;
-            scoreEl.textContent = score;
+            setScore(score - 1)
         }
     } else {
         displayMessage('شما باختید!')
@@ -38,4 +62,6 @@ const checkHandler = function() {
 }
 
 checkButton.addEventListener('click', checkHandler);
+againButton.addEventListener('click', resetGame);
+
 
