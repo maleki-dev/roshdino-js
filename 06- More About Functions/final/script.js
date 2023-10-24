b7bdc0922b5869f8e1c4c78638ae9c40c25b2149
Sajjad <saj.maleki@gmail.com>
2023-10-24 18:25:01 +0330

    complete functions

--- 06- More About Functions/final/script.js
+++ 06- More About Functions/final/script.js
@@ -3,114 +3,111 @@
 //---------------------Default Parameters---------------------
 
 const hotel = {
-    id: 125,
-    name: 'Grand',
-    book(name, count = 1) {
-        if(count < 1) {
-            console.error('count must be over 1')
-            return
-        }
-        console.log(`${name} booked ${count} bed${count === 1 ? '' : 's'} successfully!`)
-        // const bedCount = count || 1;
-        // console.log(`${name} booked ${bedCount} beds successfully!`)
+  id: 125,
+  name: "Grand",
+  book(name, count = 1) {
+    if (count < 1) {
+      console.error("count must be over 1");
+      return;
     }
-}
+    console.log(
+      `${name} booked ${count} bed${count === 1 ? "" : "s"} successfully!`
+    );
+    // const bedCount = count || 1;
+    // console.log(`${name} booked ${bedCount} beds successfully!`)
+  },
+};
 
 // hotel.book('Amir', 2);
 // hotel.book('Ali');
 
-
-
-
 //--------- Functions Accepting Callback Functions------------
 
+const convertToOneWord = (str = "") => str.replaceAll(" ", "").toLowerCase();
 
-const convertToOneWord = (str = '') => str.replaceAll(' ', '').toLowerCase();
+const upperFirstWord = (str = "") => {
+  const words = str.split(" ");
+  // return [words[0].toUpperCase(), ...words.slice(1)].join(' ');
 
-const upperFirstWord = (str = '') => {
-    const words = str.split(' ');
-    // return [words[0].toUpperCase(), ...words.slice(1)].join(' ');
-
-    const firstWord = words.splice(0, 1)[0].toUpperCase();
-    return [firstWord, ...words].join(' ');
-
-}
-
+  const firstWord = words.splice(0, 1)[0].toUpperCase();
+  return [firstWord, ...words].join(" ");
+};
 
 const transformer = (str, fn) => {
-    console.log(fn.name + ': ' + fn(str))
-    return 
-}
+  console.log(fn.name + ": " + fn(str));
+  return;
+};
 
 // transformer('i love javascript',upperFirstWord )
 // transformer('i love javascript',convertToOneWord )
 
-
 //---------------- Functions Returning Functions--------------
 
 const counter = function (step) {
-    let count = 0;
-    return function() {
-        count += step;
-        console.log(count)
-    }
-}
+  let count = 0;
+  return function () {
+    count += step;
+    console.log(count);
+  };
+};
 
 const plus1 = counter(1);
 const plus2 = counter(2);
-
 
 // plus1()
 // plus1()
 // plus1()
 // plus2()
 
-
-
-
 //---------------- The call and apply Methods--------------
 
-
 const mahanAirline = {
-    name: 'Mahan',
-    book(name, fn = 'MN08') {
-        console.log(`flight ${fn} of ${this.name} reserved by ${name}`)
-    }
+  name: "Mahan",
+  book(name, fn = "MN08") {
+    console.log(`flight ${fn} of ${this.name} reserved by ${name}`);
+  },
 };
 
 // mahanAirline.book('Sajad');
 
 const luftansa = {
-    name: 'Luftansa',
-    planes: 10,
-    buyPlane() {
-        this.planes += 1;
-    }
-}
+  name: "Luftansa",
+  planes: 10,
+};
 
 // mahanAirline.book.call(luftansa, ...['Sajjad', 'LF107']);
 // mahanAirline.book.apply(luftansa, ['Sajjad', 'LF107']);
 
-
-
 //--------------------- The bind Method--------------------
 
-const bookLuftansa = mahanAirline.book.bind(luftansa); 
+const bookLuftansa = mahanAirline.book.bind(luftansa);
 
-bookLuftansa('Sajjad', '103LF');
+bookLuftansa("Sajjad", "103LF");
 
-const buyPlaneButton = document.querySelector('.buyPlaneButton');
+const buyPlaneButton = document.querySelector(".buyPlaneButton");
 
 const buyPlaneForLuftansa = luftansa.buyPlane;
 
-buyPlaneButton.addEventListener('click', luftansa.buyPlane)
-console.log(luftansa);
-/*
+const buyPlane = function () {
+  this.planes += 1;
+  console.log(this);
+  console.log(luftansa);
+};
 
+buyPlaneButton.addEventListener("click", buyPlane.bind(luftansa));
+console.log(luftansa);
 
 //-------Immediately Invoked Function Expressions (IIFE)----
+(function ({ count }) {
+  console.log("IIFE");
+})({ count: 10 });
 
+if(true){
+  const x = 10;
+}
 
+console.log(x);
+/*
 //------------------------- Closures------------------------
 console.dir(plus1)
 console.dir(plus2)
