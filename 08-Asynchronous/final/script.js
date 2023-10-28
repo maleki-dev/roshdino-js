214353f1bc8746ff9bc09b15ef912590493f183d
Maleki <saj.maleki@gmail.com>
2023-10-28 16:12:08 +0330

    initialize session 13

---
+++ 08-Asynchronous/final/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const apiPath = "https://api.seriesquotes.10cyrilc.me";
+
+//----------------------AJAX--------------------------
+
+
+//---------------Promises and fetch api---------------
+
+
+//------------------The Event Loop--------------------
+
+
+//--------------------Async/Await--------------------
